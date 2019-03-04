58bccc74393886559a966584ab34b23e903da8e0
Jeppe Mariager-Lam <jeppe@mariager-lam.com>
2019-03-04 15:33:55 +0700

    Moves CORS outside of routes/api.php and instead used a plugin

--- routes/api.php
+++ routes/api.php
@@ -13,12 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-/* Setup CORS */
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-
-
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
