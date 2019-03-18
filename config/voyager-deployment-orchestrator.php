1e103b95d9af8029ed8f4debbed297859a4b7a53
chendaloeurt <chenda.loeurt@gmail.com>
2019-03-18 15:06:13 +0700

    update config file

--- config/voyager-deployment-orchestrator.php
+++ config/voyager-deployment-orchestrator.php
@@ -2,12 +2,7 @@
 
 return [
     'tables' => [
-<<<<<<< HEAD
-        // table names for generating bread seeders.
-       'users','sensors','locations','datapoints','usertokens'
-    ],
-];
-=======
+
         'users',
         'sensors',
         'usertokens',
@@ -17,4 +12,4 @@ return [
 ];
 
 //users,sensors,usertokens,datapoints,locations
->>>>>>> 2d9722f5439ed9ee9d1dadf9f3a9555389811861
+
