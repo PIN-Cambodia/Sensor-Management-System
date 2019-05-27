e5cfd3cd7fe9fc7fa94bf738eea7f48375d5cd15
sopheak <sopheakyong@gmail.com>
2019-05-27 18:12:51 +0700

    Update Datapoint model for allow field sensor_height,distance_report,water_height

--- app/Datapoint.php
+++ app/Datapoint.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Datapoint extends Model
 {
     //
-	protected $fillable = ['sensor_id','location_id','data'];
+	protected $fillable = ['sensor_id','location_id','data','sensor_height','distance_report','water_height'];
     public function Sensor(){
     	return $this->belongsTo('App\Sensor');
     }
